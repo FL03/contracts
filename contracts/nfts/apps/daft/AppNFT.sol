4b53e3a661703a813275c6a2cf139e83fe7f6c82
Joe McCain III <jo3mccain@icloud.com>
2023-01-08 14:51:00 +0000

    Update

--- contracts/nfts/apps/daft/AppNFT.sol
+++ contracts/nfts/apps/daft/AppNFT.sol
@@ -1,38 +1,15 @@
 /// SPDX-License-Identifier: Apache-2.0 
 pragma solidity >=0.8.0 <0.9.0;
 
+import "./IApp.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-interface IAppNFT {
-    event Authenticated(address indexed originator, string indexed appellation, uint256 tokenId);
-
-    event Registered(address indexed originator, string indexed appellation, uint256 tokenId);
-    event Registering(address indexed originator, string indexed appellation, uint256 tokenId);
-    
-    event Updated(address indexed originator, string indexed signature, uint256 cid);
-    event Updating(address indexed originator, string indexed signature, string cid);
-
-    /**
-     * @dev Retrieve the previous build
-     */
-    function getPreviousBuild(uint256 versionNumber) external view returns (string memory);
-    /**
-     * @dev Register the application with the mainnet (Reaction)
-     */
-    function register(address originator, string memory appellation, uint256 tokenId) external view returns (bool);
-    /**
-     * @dev Application tokens implement standard update methods, requiring a valid signature verified against the global registry on Reaction
-     */
-    function update(string memory signature) external view returns (bool);
-}
-
-
 /**
- * @title AppNFT
+ * 
  * @dev The base application token serving as the backbone of each portal
- *
+ * @title AppNFT
  */
 abstract contract AppNFT is IAppNFT, ERC721URIStorage {
     using Counters for Counters.Counter;
---
+++ contracts/nfts/apps/daft/IApp.sol
@@ -0,0 +1,34 @@
+/// SPDX-License-Identifier: Apache-2.0 
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+/**
+ * 
+ * @dev An interface describing the basis of each application token
+ * @title IAppNFT: 
+ */
+interface IAppNFT {
+    event Authenticated(address indexed originator, string indexed appellation, uint256 tokenId);
+
+    event Registered(address indexed originator, string indexed appellation, uint256 tokenId);
+    event Registering(address indexed originator, string indexed appellation, uint256 tokenId);
+    
+    event Updated(address indexed originator, string indexed signature, uint256 cid);
+    event Updating(address indexed originator, string indexed signature, string cid);
+
+    /**
+     * @dev Retrieve the previous build
+     */
+    function getPreviousBuild(uint256 versionNumber) external view returns (string memory);
+    /**
+     * @dev Register the application with the mainnet (Reaction)
+     */
+    function register(address originator, string memory appellation, uint256 tokenId) external view returns (bool);
+    /**
+     * @dev Application tokens implement standard update methods, requiring a valid signature verified against the global registry on Reaction
+     */
+    function update(string memory signature) external view returns (bool);
+}
