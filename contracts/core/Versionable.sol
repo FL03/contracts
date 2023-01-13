31e67caa83db7d1435ef64629316bb54ed4dddbe
FL03 <jo3mccain@icloud.com>
2023-01-13 17:46:40 -0600

    Update

--- contracts/core/Versionable.sol
+++ contracts/core/Versionable.sol
@@ -3,6 +3,15 @@ pragma solidity >=0.8.0 <0.9.0;
 
 import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
 
+library VersionControl {
+
+    struct Registry {
+        string name;
+        address owner;
+        mapping(address => mapping(uint256 => string)) registrants;
+    }
+}
+
 interface Buildable {
     event BuildRequested(address indexed originator, string indexed appellation, uint256 buildId);
 
