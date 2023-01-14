417a131e64110180e52c048eee648c564c397742
FL03 <jo3mccain@icloud.com>
2023-01-13 20:09:57 -0600

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- contracts/nfts/apps/Application.sol
+++ contracts/nfts/apps/Application.sol
@@ -15,9 +15,10 @@ import "@openzeppelin/contracts-upgradeable/utils/CountersUpgradeable.sol";
  * @dev The base application token serving as the backbone of each portal
  * @title AppNFT
  */
-contract AppNFT is IApplication, ERC721URIStorageUpgradeable, OwnableUpgradeable, UUPSUpgradeable {
+contract App is ERC721URIStorageUpgradeable, IApplication, OwnableUpgradeable, UUPSUpgradeable {
     using CountersUpgradeable for CountersUpgradeable.Counter;
     CountersUpgradeable.Counter public versions;
+    
     mapping(uint256 => string) public builds; // Each version maps to the hash of the version; similar to NixOS packages
 
     constructor(string memory _name, string memory _symbol, string memory tokenURI) {
@@ -26,34 +27,24 @@ contract AppNFT is IApplication, ERC721URIStorageUpgradeable, OwnableUpgradeable
         __UUPSUpgradeable_init();
         mint(tokenURI);
     }
-
     /// Authorize the contract upgrade
     function _authorizeUpgrade(address newImplementation) internal onlyOwner override virtual {}
     /// Fetch the previous build version given the latest release
-    function getPreviousBuild(uint256 versionNumber)
-        public
-        override
-        view
-        returns (string memory)
-    {
+    function getPreviousBuild(uint256 versionNumber) public override view returns (string memory) {
         return builds[versionNumber];
     }
     /// Implements the method of generation, minting new applications accordingly
     function mint(string memory tokenURI) private returns (uint256) {
         versions.increment();
         uint256 tokenId = 1;
-        uint256 currentVersion = versions.current();
+        uint256 currentVersion = version();
         _mint(msg.sender, tokenId);
         _setTokenURI(tokenId, tokenURI);
         builds[currentVersion] = tokenURI;
         return tokenId;
     }
     /// Implements a standard method of transfer
-    function transferFrom(
-        address from,
-        address to,
-        uint256 tokenId
-    ) public virtual override {
+    function transferFrom(address from, address to, uint256 tokenId) public virtual override {
         require(
             _isApprovedOrOwner(_msgSender(), tokenId),
             "ERC721: caller is not token owner nor approved"
@@ -74,5 +65,5 @@ contract AppNFT is IApplication, ERC721URIStorageUpgradeable, OwnableUpgradeable
     function version() public view returns (uint256) {
         return versions.current();
     }
-
+    
 } 
