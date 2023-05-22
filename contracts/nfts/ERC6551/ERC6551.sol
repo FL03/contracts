af525140e7d926af721dbcb262cbbacb300b9a1e
FL03 <jo3mccain@icloud.com>
2023-05-22 17:38:42 -0500

    update

---
+++ contracts/nfts/ERC6551/ERC6551.sol
@@ -0,0 +1,4 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
---
+++ contracts/nfts/ERC6551/IERC6551.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity >=0.8.0 <0.9.0;
+
+import "@openzeppelin/contracts/utils/introspection/IERC165.sol";
+
+
+/**
+ * @custom:url https://eips.ethereum.org/EIPS/eip-6551
+ * @title IERC6551: Non-fungible Token Bound Accounts
+ */
+interface IERC6551 is IERC165 {
+    
+}
